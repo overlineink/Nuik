749e9d1f68e77d70991c513f0621438681e2f081
Jorge Dacosta <jorgemsdc@mail.com>
2019-05-21 00:35:46 +0100

    fix: remove unused imports

--- app/src/main/java/com/example/user/nuik/StudentsActivity.java
+++ app/src/main/java/com/example/user/nuik/StudentsActivity.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.widget.ProgressBar;
 
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
